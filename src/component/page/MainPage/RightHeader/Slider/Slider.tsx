24d0763d8149c76d9a8ac4f5520f3421360b428a
bongjoonkim <bongjoonkim96@gmail.com>
2024-05-26 11:34:23 +0900

    사용자 정보 관련 띄워주는 슬라이더 popover 만드는 중

---
+++ src/component/page/MainPage/RightHeader/Slider/Slider.tsx
@@ -0,0 +1,23 @@
+import styled from "styled-components";
+
+interface SliderProps {
+
+};
+
+function Slider(props: SliderProps) {
+  
+  return (
+    <StyledSlider>
+    
+    </StyledSlider>
+  )
+};
+
+export default Slider;
+
+const StyledSlider = styled.div`
+  width: 20rem;
+  height: 4rem;
+  position: absolute;
+  background: whitesmoke;
+`;
