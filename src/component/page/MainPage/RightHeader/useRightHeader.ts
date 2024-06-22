f25a769b04e4e483413fc9074c95cef98553844d
bongjoonkim <bongjoonkim96@gmail.com>
2024-06-22 18:24:50 +0900

    endpoint 공통화 코드 개발 완료

--- src/component/page/MainPage/RightHeader/useRightHeader.ts
+++ src/component/page/MainPage/RightHeader/useRightHeader.ts
@@ -4,19 +4,32 @@ import {AccessToken} from "../../../../stores/jotai/jotai";
 import {loadable} from "jotai/utils";
 import {getUserData} from "../../../../endpoints/users-endpoints";
 import {useAuth} from "../../../../appConfig/AuthContext";
-import {getAllMenus, getAllMenus2} from "../../../../endpoints/menus-endpoints";
+import {getAllMenus, getAllMenus2, getAllMenus3} from "../../../../endpoints/menus-endpoints";
 import {useRecoilState} from "recoil";
 import recoil from "../../../../stores/recoil";
+import {useNavigate} from "react-router-dom";
+import {endpointUtils} from "../../../../utils/endpointUtils";
 
 
 function useRightHeader() {
   const [isSliderOpen ,setSliderOpen] = useState<boolean>(false);
   const [errorMsg, setErrorMsg] = useRecoilState(recoil.alertMsg);
   const {accessToken, setAccessToken} = useAuth();
+  const navigate = useNavigate();
   
-  const handleAvatarClick = useCallback((event : MouseEvent<HTMLSpanElement>) => {
+  const handleAvatarClick = useCallback(async (event : MouseEvent<HTMLSpanElement>) => {
     setSliderOpen(prev => !prev);
-    getAllMenus2(accessToken, setAccessToken);
+    try {
+      // await getAllMenus2(accessToken, setAccessToken);
+      // await getAllMenus3(accessToken, setAccessToken);
+      console.log("엑세스 토큰", accessToken)
+      await endpointUtils.authAxios(getAllMenus3, accessToken, setAccessToken);
+    } catch (e) {
+      console.log("여기는 안 오냐?", e)
+      if (e === "refreshToken expired") {
+        navigate("/login")
+      }
+    }
   }, [isSliderOpen]);
   
   const getUserInfo = useCallback(async () => {
