676da0f1e268fd91af1545b6f690245e83f95bdb
bongjoonkim <bongjoonkim96@gmail.com>
2024-07-24 00:18:30 +0900

    블로그 제목 작성

--- src/common/elements/textField/CusInput/CusInput.tsx
+++ src/common/elements/textField/CusInput/CusInput.tsx
@@ -1,13 +1,23 @@
 import {Input, InputProps} from "@chakra-ui/react";
+import {useCallback, useEffect, useState} from "react";
 
 interface CusInputProps extends InputProps {
 
 }
 
 function CusInput(props : CusInputProps) {
+  const [value, setValue] = useState<any>(props.value || "");
+  console.log("값 확인", value)
+  
+  const handleChange = useCallback((event : any) => {
+    setValue(event.target.value);
+  }, [props.value, value]);
+  
   return (
     <Input
       {...props}
+      value={props.value}
+      onChange={props.onChange}
     />
   )
 }
--- src/common/layout/BlogLayout/Documents/MakingDocLayout/useMakeDocLayout.ts
+++ src/common/layout/BlogLayout/Documents/MakingDocLayout/useMakeDocLayout.ts
@@ -2,5 +2,12 @@ import {MakeDocLayoutProps} from "./MakeDocLayout";
 import {useState} from "react";
 
 function useMakeDocLayout(props : MakeDocLayoutProps) {
-  const [document, setDocument] = useState()
+  const [document, setDocument] = useState<DocumentDTO | any>();
+  
+  return {
+    document,
+    setDocument
+  }
 }
+
+export default useMakeDocLayout;
